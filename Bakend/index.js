d2fe179f2842aa1100be24f9e1e4a7e491111ba1
YashSarag <mathslover3.1415@gmail.com>
2025-09-19 20:04:46 +0530

    Update index.js

--- Bakend/index.js
+++ Bakend/index.js
@@ -14,10 +14,10 @@ app.use(cors({
 
 
 const path = require('path');
-app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.use(express.static(path.join(__dirname, 'Frontend/build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Frontend/build', 'index.html'));
 });
 
 
